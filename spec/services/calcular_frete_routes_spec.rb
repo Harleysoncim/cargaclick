9ca7cba4a54a1fdd86ed6a1618b29726e8924f8c
Harley José Soncim <harleysoncim@gmail.com>
2026-09-11 00:26:22 -0300

    test: corrige mocks da rota ORS

--- spec/services/calcular_frete_routes_spec.rb
+++ spec/services/calcular_frete_routes_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe CalcularFrete, "OpenRouteService JSON responses" do
   let(:http) { instance_double(Net::HTTP) }
   let(:params) { { origem: "São Paulo, SP", destino: "Santos, SP", peso: "10,5", volume: "1,25" } }
   let(:route_body) do
-        geocode_origin = Net::HTTPOK.new("1.1", "200", "OK")
-        geocode_destination = Net::HTTPOK.new("1.1", "200", "OK")
-        allow(geocode_origin).to receive(:body).and_return({ features: [{ geometry: { coordinates: [-46.63, -23.55] } }] }.to_json)
-        allow(geocode_destination).to receive(:body).and_return({ features: [{ geometry: { coordinates: [-46.32, -23.96] } }] }.to_json)
-        allow(http).to receive(:request).and_return(geocode_origin, geocode_destination, response)
+    {
+      "routes" => [{
+        "summary" => { "distance" => 75_572.6, "duration" => 4_500 },
+        "geometry" => { "type" => "LineString", "coordinates" => [[-46.63, -23.55], [-46.32, -23.96]] }
+      }]
+    }
   end
   let(:response) { Net::HTTPOK.new("1.1", "200", "OK") }
 
@@ -19,14 +20,13 @@ RSpec.describe CalcularFrete, "OpenRouteService JSON responses" do
     allow(ENV).to receive(:[]).with("OPENROUTESERVICE_API_KEY").and_return("test-only-route-key")
     allow(Net::HTTP).to receive(:new).with("api.openrouteservice.org", 443).and_return(http)
     allow(http).to receive(:use_ssl=).with(true)
-        expect(result[:destino_coords]).to eq([-46.32, -23.96])
+    allow(http).to receive(:open_timeout=)
     allow(http).to receive(:read_timeout=)
-    allow(http).to receive(:request).and_return(
-      Net::HTTPOK.new("1.1", "200", "OK").tap do |geocode|
-        allow(geocode).to receive(:body).and_return({ features: [{ geometry: { coordinates: [-46.63, -23.55] } }] }.to_json)
-      end,
-      response
-    )
+    geocode_origin = Net::HTTPOK.new("1.1", "200", "OK")
+    geocode_destination = Net::HTTPOK.new("1.1", "200", "OK")
+    allow(geocode_origin).to receive(:body).and_return({ features: [{ geometry: { coordinates: [-46.63, -23.55] } }] }.to_json)
+    allow(geocode_destination).to receive(:body).and_return({ features: [{ geometry: { coordinates: [-46.32, -23.96] } }] }.to_json)
+    allow(http).to receive(:request).and_return(geocode_origin, geocode_destination, response)
     allow(response).to receive(:body).and_return(route_body.to_json)
   end
 
@@ -36,7 +36,7 @@ RSpec.describe CalcularFrete, "OpenRouteService JSON responses" do
     expect(result).to include(sucesso: true, distancia_km: 75.57, valor_total: BigDecimal("188.93"), duracao_minutos: 75.0)
     expect(result[:rota_geojson]["type"]).to eq("LineString")
     expect(result[:origem_coords]).to eq([-46.63, -23.55])
-    expect(result[:destino_coords]).to eq([-46.63, -23.55])
+    expect(result[:destino_coords]).to eq([-46.32, -23.96])
     expect(result[:breakdown]).to include(peso: BigDecimal("10.5"), volume: BigDecimal("1.25"), subtotal_km: BigDecimal("188.93"))
     expect(http).to have_received(:request).with(satisfy { |request|
       request.path == "/v2/directions/driving-car" &&
